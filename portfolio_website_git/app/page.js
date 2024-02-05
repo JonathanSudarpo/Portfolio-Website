f5d880eccdd28f17ae4db5677e14c6183cffc514
JonathanSudarpo <116526890+JonathanSudarpo@users.noreply.github.com>
2024-02-05 16:10:20 -0500

    Portfolio website

---
+++ portfolio_website_git/app/page.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
+import AboutSection from "./components/AboutSection"
+import ProjectSection from "./components/ProjectSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
+
+export default function Home() {
+  return (
+    // Container is for different platforms. Mx-auto is for auto margins. 
+    <main className="flex min-h-screen flex-col bg-[#121212]" >
+
+      <Navbar />
+
+      <div class="container mt-24 mx-auto px-12 py-4" >
+        
+        <HeroSection />
+        <AchievementsSection/>
+        <AboutSection/>
+        <ProjectSection/>
+        <EmailSection/>
+      </div>
+      <Footer />
+    </main>
+  );
+}
