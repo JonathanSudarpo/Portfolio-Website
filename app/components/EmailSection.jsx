1c1fe849189040ce329532473a2508a562d7707f
Jonathan Tobias Sudarpo <116526890+JonathanSudarpo@users.noreply.github.com>
2024-02-06 01:46:36 -0500

    Update EmailSection.jsx

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -43,7 +43,6 @@ const EmailSection = () => {
 
     return (
         <section id = "contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap=4 relative">
-            <div className = "bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
             <div className="z-10">
                 <h2 className="text-xl font-bold text-white my-2"> Lets Connect</h2>
                 <p className="text-[#ADB7BE] mb-4 max-w-md">Currently looking for Co-op opportunities, and my Email is always open!</p>
